1e8bb89f698de7a4793a0c1fc1cce93c0c52b746
Suryakant Jadhav <>
2025-01-21 13:12:32 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement("div",{ id: "parent" },[
+        React.createElement("div",{ id: "child 1" },[
+             React.createElement( "h1",{},"I am an h1 tag"), React.createElement("h2",{},"I am an h2 tag")]
+        ),
+        React.createElement( "div",{ id: "child 2" },[
+             React.createElement( "h1",{},"I am an h1 tag"), React.createElement("h2",{},"I am an h2 tag")]
+        )]
+);
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
